b182b602ff3ded81b105e545201fa8fee6cb0cbe
Fang Ling <61139150+fang-ling@users.noreply.github.com>
2023-06-16 14:50:13 +0800

    Introduce UnionFindSet to handle relationships

--- Package.swift
+++ Package.swift
@@ -5,18 +5,20 @@ import PackageDescription
 let package = Package(
   name: "pxm",
   dependencies: [
-    .package(url: "https://github.com/fang-ling/image-codec", from: "0.0.5"),
+    .package(url: "https://github.com/fang-ling/image-codec", from: "0.0.6"),
     .package(
       url: "https://github.com/fang-ling/image-transformation",
       from: "0.0.1"
-    )
+    ),
+    .package(url: "https://github.com/fang-ling/collections", from: "0.0.2")
   ],
   targets: [
     .executableTarget(
       name: "pxm",
       dependencies: [
         .product(name: "txt", package: "image-codec"),
-        .product(name: "lml", package: "image-transformation")
+        .product(name: "lml", package: "image-transformation"),
+        .product(name: "xhl", package: "collections")
       ]),
     .testTarget(
       name: "pxmTests",
--- Sources/pxm/pxm.swift
+++ Sources/pxm/pxm.swift
@@ -1,5 +1,6 @@
 import Foundation
 import txt
+import xhl
 
 @main
 public struct pxm {
@@ -31,47 +32,54 @@ public struct pxm {
         }
 
         /* Comparison */
-        var same = [String]()
-        var similar = [String]()
-        var distance = 0
-        var results = Set<String>()
-        var result = ""
+        var same = UnionFindSet<String>()
+        var similar = UnionFindSet<String>()
+        for file in files {
+            same.make_set(file)
+            similar.make_set(file)
+        }
         for i in files {
-            same.removeAll()
-            similar.removeAll()
             for j in files {
                 if (i == j) {
                     continue
                 }
-                distance = hamming_distance(hashes[i]!, hashes[j]!)
-                if distance == 0 {
-                    same.append(j)
+                let distance = hamming_distance(hashes[i]!, hashes[j]!)
+                if distance == 0 { /* File i and j are equal */
+                    same.union(i, j)
                 } else if distance <= 5 {
-                    similar.append(j)
+                    similar.union(i, j)
                 }
-            }
-            if (same.isEmpty && similar.isEmpty) {
-                continue
-            }
-            result = "Same: open"
-            same.append(i) /// Self is the same as self.
-            if same.count > 1 {
-                for j in same.sorted() {
-                    result += " \"\(j)\""
-                }
-                results.insert(result)
-            }
-            result = "Similar: open"
-            similar.append(i)
-            if similar.count > 1 {
-                for j in similar.sorted() {
-                    result += " \"\(j)\""
-                }
-                results.insert(result)
             }
         }
-        for i in results {
-            print(i)
+        var result_same = [String : Set<String>]()
+        var result_similar = [String : Set<String>]()
+        for file in files {
+            let set = same.find_set(file)
+            if result_same[set] == nil {
+                result_same[set] = Set<String>()
+            }
+            result_same[set]!.insert(file)
+        }
+        for file in files {
+            let set = similar.find_set(file)
+            if result_similar[set] == nil {
+                result_similar[set] = Set<String>()
+            }
+            result_similar[set]!.insert(file)
+        }
+        print("Same:")
+        for i in result_same {
+            if i.value.count > 1 {
+                print("open ", terminator: "")
+                print(i.value.map {"'\($0)'"}.joined(separator: " "))
+            }
+        }
+        print("Similar:")
+        for i in result_similar {
+            if i.value.count > 1 {
+                print("open ", terminator: "")
+                print(i.value.map {"'\($0)'"}.joined(separator: " "))
+            }
         }
     }
 }
